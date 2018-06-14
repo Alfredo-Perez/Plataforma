97bcf28920df9c7da0e8c400097f433ff8033c18
Cristopher K. Pinzón Resendiz <cristopher.pinzon@gmail.com>
2018-06-13 23:28:33 -0500

    tinymce local

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
-import { Editor } from '@tinymce/tinymce-react';
+//import { Editor } from '@tinymce/tinymce-react';
 
 import Posts from './components/Posts';
 import PostForm from './components/Postform';
 
 import store from './store';
+import TinyEditorComponent from './components/Editor';
 
 class App extends Component {
-  handleEditorChange = (e) => {
-    console.log('Content was updated:', e.target.getContent());
-  }
-
+  
+  
   render() {
     return (
       
@@ -24,19 +23,14 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
           </header>
-          <Editor
-            initialValue="<p>This is the initial content of the editor</p>"
-            init={{
-              plugins: 'link image code',
-              toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
-            }}
-            onChange={this.handleEditorChange}
+          <TinyEditorComponent
+            id="Example"
           />
           <PostForm />
           <hr />
           <Posts />
         </div>
-      </Provider>
+</Provider>
     );
   }
 }
