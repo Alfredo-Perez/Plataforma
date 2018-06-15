c3c0972247d27f6abfd6537414c2bcf2ff9eea8c
Cristopher K. Pinzón Resendiz <cristopher.pinzon@gmail.com>
2018-06-14 23:10:46 -0500

    comments funcionando

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -9,21 +9,48 @@ import 'tinymce/plugins/table';
 class TinyEditorComponent extends Component {
   constructor() {
     super();
-    this.state = { editor: null };
+    this.state = { editor: null , comments :[] };
   }
+
+  onEditorChange(e){
+    console.log(e)
+  }
+
   componentDidMount() {
 
    
     tinymce.init({
       selector: `#${this.props.id}`,
       skin_url: `${process.env.PUBLIC_URL}/skins/lightgray`,
-      plugins: 'wordcount table',
+      // plugins: 'tma_annotate',
+      external_plugins: {
+        'tma_annotate': `${process.env.PUBLIC_URL}/tma_annotate.js`,
+      },
+      toolbar1: 'tma_annotate tma_annotatedelete tma_annotatehide ',
+      comments : [],
+
+
       setup: editor => {
-        this.setState({ editor });
-        editor.on('keyup change', () => {
-          const content = editor.getContent();
-          this.props.onEditorChange(content);
-        });
+        this.setState({ editor : editor });
+
+        function addComment(id,text){
+            var comments = this.state.comments.slice();    
+            comments.push({id: id, text: text});   
+            this.setState({comments:comments})
+        }
+
+        function removeComment(id){
+            var comments = this.state.comments.slice();    
+            comments = comments.filter(item =>{
+                return item.id != id;
+            })
+            this.setState({comments:comments})
+        }
+
+        editor.addComment = addComment.bind(this);
+        editor.removeComment = removeComment.bind(this);
+
+      
       }
     });
   }
@@ -34,11 +61,13 @@ class TinyEditorComponent extends Component {
 
   render() {
     return (
+      <div>
       <textarea
         id={this.props.id}
         value={this.props.content}
         onChange={e => console.log(e)}
       />
+      </div>
     );
   }
 }
